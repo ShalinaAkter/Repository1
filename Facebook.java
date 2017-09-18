98f4da95efd8addb35d13a3dd4a4250773d41660
ShalinaAkter <alimm9297@gmail.com>
2017-09-17 21:23:08 -0400

    Facebook 2nd time modified

--- Facebook.java
+++ Facebook.java
@@ -56,6 +56,8 @@ public class Facebook {
 	public void CloseBrowser(){
 		System.out.println("Facebook SignUp Successful");
 		System.out.println("Facebook SignUp Successful");
+		System.out.println("Facebook SignUp Successful");
+		System.out.println("Facebook SignUp Successful");
 		driver.close();
 		
 	}
